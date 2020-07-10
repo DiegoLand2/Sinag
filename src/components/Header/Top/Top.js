5a2f37c51f5c5a475d2558f1ea2f298c93ae432c
Aaron Melian <aaronimelian@gmail.com>
2020-07-11 00:56:21 +0200

    Logo fixes

--- src/components/Header/Top/Top.js
+++ src/components/Header/Top/Top.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Top.module.css'
+import classes from "./Top.module.scss";
 
-const top = () => (
-    <header className={classes.Top}>
-        Vibes & Design
-    </header>
-)
+const top = () => <header className={classes.Top}>Vibes & Design</header>;
 
 export default top;
--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,26 +1,45 @@
-import React from 'react'
+import React from "react";
 
-import classes from './SideDrawer.module.scss'
+import classes from "./SideDrawer.module.scss";
 
-const sideDrawer = props => {
-    
-    // let drawerClasses = `${classes.SideDrawer}`
-    // if (props.show) {
-    //     drawerClasses =  `${classes.SideDrawer} ${classes.open}`
-    // }
+const sideDrawer = (props) => {
+  // let drawerClasses = `${classes.SideDrawer}`
+  // if (props.show) {
+  //     drawerClasses =  `${classes.SideDrawer} ${classes.open}`
+  // }
 
-    return (
-        <nav className={`${classes.SideDrawer} ${props.show ? classes.open : ""}`}>
-        {/* <nav className={drawerClasses}> */}
-            <ul>
-                <li><a href='/'>Quiénes somos</a></li>
-                <li><a href='/'>Tienda</a></li>
-                <li><a href='/'>Japamalas</a></li>
-                <li><a href='/'>Kokedamas</a></li>
-                <li><a href='/'>Blog</a></li>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className={`${classes.SideDrawer} ${props.show ? classes.open : ""}`}>
+      {/* <nav className={drawerClasses}> */}
+      <ul>
+        <li className={classes.item}>
+          <a className={classes.itemText} href="/">
+            Quiénes somos
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a className={classes.itemText} href="/">
+            Tienda
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a className={classes.itemText} href="/">
+            Japamalas
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a className={classes.itemText} href="/">
+            Kokedamas
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a className={classes.itemText} href="/">
+            Blog
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default sideDrawer;
